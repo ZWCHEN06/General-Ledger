c60b9c02f6813d5482d1bb36c976cb6994bdeb65
ZWCHEN06 <1548601713@qq.com>
2026-06-27 10:35:57 +0800

    启动时初始化数据库

--- expense-tracker-qt/main.cpp
+++ expense-tracker-qt/main.cpp
@@ -1,9 +1,19 @@
+#include "src/database/DatabaseManager.h"
+
+#include <QDebug>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    DatabaseManager databaseManager;
+    if (!databaseManager.openDatabase()) {
+        qWarning().noquote() << "打开 SQLite 数据库失败，界面将继续启动。";
+    } else if (!databaseManager.initializeTables()) {
+        qWarning().noquote() << "初始化数据库表失败，界面将继续启动。";
+    }
 
     QQmlApplicationEngine engine;
     QObject::connect(
